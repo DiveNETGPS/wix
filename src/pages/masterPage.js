1cf1302400be26a06d064511a6e9bf352a929ffb
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-07-03 06:51:55 +0000

    Initial commit

---
+++ src/pages/masterPage.js
@@ -0,0 +1,15 @@
+// The code in this file will load on every page of your site
+
+$w.onReady(function () {
+	// Write your code here
+});
+
+/**
+*	Adds an event handler that runs when an element is displayed
+ in the viewable part of the current window.
+*	 @param {$w.Event} event
+*/
+export function image77_viewportEnter(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
