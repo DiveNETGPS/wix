5b06f435647580987ea8fe78cfb7f420bd8db198
genebivol <208720151+genebivol@users.noreply.github.com>
2026-04-12 11:24:08 -0700

    Start refactored site version: code cleanup and structure improvements

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,15 +1,6 @@
 // The code in this file will load on every page of your site
 
 $w.onReady(function () {
-	// Write your code here
+	console.log("Dev Mode is working: masterPage onReady ran");
 });
 
-/**
-*	Adds an event handler that runs when an element is displayed
- in the viewable part of the current window.
-*	 @param {$w.Event} event
-*/
-export function image77_viewportEnter(event) {
-	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
-	// Add your code for this event here: 
-}
